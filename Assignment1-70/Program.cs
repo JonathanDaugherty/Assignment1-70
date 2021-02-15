5ee0e18f9493edd90000c0fd288d158887090d1b
jmdaugherty94@gmail.com <jmdaugherty94@gmail>
2021-02-15 10:15:41 -0500

    Took out the comments

--- Assignment1-70/Program.cs
+++ Assignment1-70/Program.cs
@@ -3,12 +3,12 @@
 namespace Assignment1_70 {
     class Program {
         static void Main(string[] args) {
-            /*var total = 0;
+            var total = 0;
             for (var idx = 1; idx < 71; idx++)
                 if (idx % 7 == 0) {
                     total += idx;
                 }
-                    Console.WriteLine($"Total is {total}"); */
+                    Console.WriteLine($"Total is {total}"); 
 
              int[] nbrs = { 754, 697, 823, 6, 235, 543, 908, 123, 450, 768, 999, 630 };
              var highest = 0;
